1b1d30a714be3852597223e9b032ec81c0cbe4ea
mjlbnu <mjlbnu@gmail.com>
2020-01-22 22:26:14 -0300

    database config

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const express = require('express');
-const db = require('./queries')
+const bodyParser = require('body-parser');
+const db = require('./queries');
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello Omnistack2' });
-})
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-app.get('/users', db.getUsers)
-app.get('/users/:id', db.getUserById)
-app.post('/users', db.createUser)
-app.put('/users/:id', db.updateUser)
-app.delete('/users/:id', db.deleteUser)
+app.get('/', (request, response) => {
+  return response.json({ info: 'Node.js, Express and Postgres API' });
+});
+
+app.get('/users', db.getUsers);
+app.get('/users/:id', db.getUserById);
+app.post('/users', db.createUser);
+app.post('/users2', db.createUser2);
+app.put('/users/:id', db.updateUser);
+app.delete('/users/:id', db.deleteUser);
 
 app.listen(3333);
